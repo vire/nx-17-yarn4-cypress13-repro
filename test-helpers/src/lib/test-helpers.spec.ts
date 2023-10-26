fb2965ba2290c07f863483f53f5ad7ccdd39812a
Viliam Elischer <code.vire@gmail.com>
2023-10-26 10:44:49 +0200

    add test-helpers library

---
+++ test-helpers/src/lib/test-helpers.spec.ts
@@ -0,0 +1,7 @@
+import { testHelpers } from './test-helpers';
+
+describe('testHelpers', () => {
+  it('should work', () => {
+    expect(testHelpers()).toEqual('test-helpers');
+  });
+});
---
+++ test-helpers/src/lib/test-helpers.ts
@@ -0,0 +1,3 @@
+export function testHelpers(): string {
+  return 'test-helpers';
+}
